22a65ce6d06fbb8be73cb303e7913309728caf4a
Josie <jandersoj03@gmail.com>
2024-12-07 14:18:48 -0500

    base code

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function App() {
+            
+    return (
+        <>
+            <header>
+                <h1>Crochet setup</h1>
+            </header>
+            <main>
+                <p>Main</p>
+            </main>
+        </>
+    );
+}
